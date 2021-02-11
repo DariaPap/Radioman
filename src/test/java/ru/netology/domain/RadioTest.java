3f6f0c7b95e82d1682de0aae686bebf6b68b7566
Daria Papunina <popunina@mail.ru>
2021-02-11 05:59:32 +0300

    tests fixed

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -11,7 +11,7 @@ class RadioTest {
         Radio radio = new Radio();
         radio.setVolume(0);
         radio.volumeUp();
-        assertEquals(1, 1);
+        assertEquals(1, radio.getVolume());
     }
 
     @Test
@@ -19,7 +19,7 @@ class RadioTest {
         Radio radio = new Radio();
         radio.setVolume(10);
         radio.volumeUp();
-        assertEquals(10, 10);
+        assertEquals(10, radio.getVolume());
     }
 
     @Test
@@ -27,7 +27,7 @@ class RadioTest {
         Radio radio = new Radio();
         radio.setVolume(1);
         radio.volumeDown();
-        assertEquals(0, 0);
+        assertEquals(0, radio.getVolume());
     }
 
     @Test
@@ -35,7 +35,7 @@ class RadioTest {
         Radio radio = new Radio();
         radio.setVolume(0);
         radio.volumeDown();
-        assertEquals(0, 0);
+        assertEquals(0, radio.getVolume());
     }
 
     @Test
@@ -43,7 +43,7 @@ class RadioTest {
         Radio radio = new Radio();
         radio.setStationNumber(0);
         radio.stationUp();
-        assertEquals(1, 1);
+        assertEquals(1, radio.getStationNumber());
     }
 
     @Test
@@ -51,7 +51,7 @@ class RadioTest {
         Radio radio = new Radio();
         radio.setStationNumber(9);
         radio.stationUp();
-        assertEquals(0, 0);
+        assertEquals(0, radio.getStationNumber());
     }
 
     @Test
@@ -59,7 +59,7 @@ class RadioTest {
         Radio radio = new Radio();
         radio.setStationNumber(9);
         radio.stationDown();
-        assertEquals(8, 8);
+        assertEquals(8, radio.getStationNumber());
     }
 
     @Test
@@ -67,6 +67,6 @@ class RadioTest {
         Radio radio = new Radio();
         radio.setStationNumber(0);
         radio.stationDown();
-        assertEquals(9, 9);
+        assertEquals(9, radio.getStationNumber());
     }
 }
